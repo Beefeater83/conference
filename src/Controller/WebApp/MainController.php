563df71b0e33162971bde13084232713bbdf75df
sergey dyakonov <beefeater83@gmail.com>
2025-07-19 11:40:55 +0300

    mob menu

---
+++ src/Controller/WebApp/MainController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\WebApp;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    public function __construct() {}
+
+    #[Route('/{_locale}', name: 'main_page', requirements: ['_locale' => 'en|ru'], defaults: ['_locale' => 'en'], methods: ['GET'])]
+    public function mainPage(Request $request): Response
+    {
+        $conference = 'Grain 2025';
+
+        return $this->render('components/main_page.html.twig', [
+            'data' => $conference
+        ]);
+    }
+}
